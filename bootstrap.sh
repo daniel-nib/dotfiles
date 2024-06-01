2b147e575be858852bd049da6e4d14a416463a55
Danie <170895260+daniel-nib@users.noreply.github.com>
2024-06-01 22:26:59 +1200

    add file checker

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,18 +19,18 @@ doIt;
 unset doIt;
 
 
-git clone https://github.com/tom-auger/cmdtime ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/cmdtime
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com:grigorii-zander/zsh-npm-scripts-autocomplete ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-npm-scripts-autocomplete
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+[ ! -f "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/cmdtime" ] && git clone https://github.com/tom-auger/cmdtime ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/cmdtime
+[ ! -f "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ] && git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+[ ! -f "~/.zsh/zsh-syntax-highlighting" ] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --all
+if [ ! -f "~/.fzf" ]; then
+	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+	~/.fzf/install --all
+fi
 
 # installs fnm (Fast Node Manager)
 curl -fsSL https://fnm.vercel.app/install | bash
 # download and install Node.js
 fnm use --install-if-missing 22
-
 
 sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
